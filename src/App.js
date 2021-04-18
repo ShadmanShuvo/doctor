04f9601bd1d7ebace8c2c9f4661a8ad6bc59d962
ShadmanShuvo <smsis404@gmail.com>
2021-04-18 21:08:11 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './components/Home/Home/Home';
+import Appointment from './components/Appointment/Appointment/Appointment';
+
+
+
+
+function App() {
+
+  return (
+
+    <Router>
+      <Switch>
+        <Route path="/appointment">
+          <Appointment></Appointment>
+        </Route>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+      </Switch>
+    </Router>
+
+  );
+}
+
+export default App;
